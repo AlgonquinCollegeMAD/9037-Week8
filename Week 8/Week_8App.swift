43e7704a41c72e27fd80fbca21bc9d9ddf375064
“Vladimir <vladimircezar@icloud.com>
2023-09-27 19:13:50 -0400

    Initial Commit

---
+++ Week 8/Week_8App.swift
@@ -0,0 +1,17 @@
+//
+//  Week_8App.swift
+//  Week 8
+//
+//  Created by Vladimir Cezar on 2023-09-27.
+//
+
+import SwiftUI
+
+@main
+struct Week_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
