fd2c7783a9963e57000247153d76bf9e4ad6aa0b
“Vladimir <vladimircezar@icloud.com>
2023-09-27 20:50:46 -0400

    Initial Commit

--- Week 8/ContentView.swift
+++ Week 8/ContentView.swift
@@ -1,24 +1,70 @@
-//
-//  ContentView.swift
-//  Week 8
-//
-//  Created by Vladimir Cezar on 2023-09-27.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+
+enum TrafficLight {
+  case red
+  case yellow
+  case green
+  
+  func next() -> TrafficLight {
+    switch self {
+    case .red:
+      return .green
+    case .yellow:
+      return .red
+    case .green:
+      return .red
     }
+  }
+}
+
+
+struct ContentView: View {
+  var light: TrafficLight  = .red
+  
+  var redColor: Color {
+    light == .red ? Color.red : Color.red.opacity(0.4)
+  }
+  
+  var yellowColor: Color {
+    light == .yellow ? Color.yellow : Color.yellow.opacity(0.4)
+  }
+  
+  var greenColor: Color {
+    light == .green ? Color.green : Color.green.opacity(0.4)
+  }
+  
+  var body: some View {
+    ZStack() {
+      RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+        .frame(width: 200, height: 400)
+      VStack() {
+        Circle()
+          .frame(width: 100)
+          .foregroundColor(redColor)
+        Circle()
+          .frame(width: 100)
+          .foregroundColor(yellowColor)
+        Circle()
+          .frame(width: 100)
+          .foregroundColor(greenColor)
+      }
+    }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
+}
+
+#Preview {
+  ContentView(light: .red)
+}
+
+#Preview {
+  ContentView(light: .yellow)
+}
+
+#Preview {
+  ContentView(light: .green)
 }
--- Week 8/Week_8App.swift
+++ Week 8/Week_8App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Week_8App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+          ContentView(light: .green)
         }
     }
 }
